4decbdf7e351889a9265486526efa63ddf04805b
Ola <lanreprincess@gmail.com>
2024-08-19 00:56:21 +0100

    modified css and added extra features

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,57 @@
 import { UserOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 
-export const NavBar = () => {
+export const NavBar = ({ inputValue, setInputValue }) => {
+  function handleInput(e) {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  }
   return (
     <>
       <div className="navbar bg-white border-b-2">
-        <div className="btn btn-ghost btn-circle avatar cartIcon">
-          <ShoppingCartOutlined />
-        </div>
-        <div className="flex-1 text-center justify-center">
+        <div className="flex-1">
           <a className="btn btn-ghost text-xl text-green-500">BookOasis</a>
         </div>
 
         <div className="flex-none gap-2">
-          {/* <div className="form-control">
+          <div className="form-control">
             <input
               type="text"
               placeholder="Search"
+              value={inputValue}
+              onChange={handleInput}
               className="input input-bordered w-24 md:w-auto bg-gray-100"
             />
-          </div> */}
+          </div>
           <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost rounded-btn text-green-500"
+            >
+              Category
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content bg-base-100 rounded-box z-[1] mt-4 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Fiction</a>
+              </li>
+              <li>
+                <a>Science</a>
+              </li>
+              <li>
+                <a>Romance</a>
+              </li>
+              <li>
+                <a>HIstory</a>
+              </li>
+            </ul>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div className="btn btn-ghost btn-circle avatar cartIcon">
+              <ShoppingCartOutlined />
+            </div>
             <div
               tabIndex={0}
               role="button"
