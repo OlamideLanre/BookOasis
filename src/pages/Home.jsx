c5c66e4b710919b50f8024d4c785c68aaf51769e
Ola <lanreprincess@gmail.com>
2024-08-19 00:54:26 +0100

    edited CSS,code clean up &  displayed data on UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { LoadingOutlined } from "@ant-design/icons";
 import "./Home.css";
 export const Home = ({ inputValue }) => {
   const [loading, setLoading] = useState(true);
-  const [bookDetails, setBookDetails] = useState();
+  const [bookDetails, setBookDetails] = useState([]);
 
   const myHeaders = new Headers();
   myHeaders.append("Accept", "application/json");
@@ -20,20 +20,21 @@ export const Home = ({ inputValue }) => {
     redirect: "follow",
   };
 
-  const URL_REQUEST =
-    "https://Big-Book-API.proxy-production.allthingsdev.co/search-books?query=books+about+wizards";
+  const URL_REQUEST = `https://Big-Book-API.proxy-production.allthingsdev.co/search-books?query=books+about+${inputValue}&number=100`;
   const getBooks = async () => {
     try {
       let response = await fetch(URL_REQUEST, requestOptions);
-      console.log(response);
+      let data = await response.json();
+      const details = data.books.flat();
+      setBookDetails(details);
+      if (bookDetails != []) {
+        setLoading(false);
+        console.log(bookDetails);
+      }
       if (!response.ok) {
         console.log(
           "response not okay, here's the status code: " + response.status
         );
-      } else {
-        setLoading(false);
-        let data = await response.json();
-        console.log(data);
       }
     } catch (error) {
       console.log("error msg: " + error);
@@ -41,20 +42,35 @@ export const Home = ({ inputValue }) => {
   };
   useEffect(() => {
     getBooks();
-  }, []);
+  }, [inputValue]);
 
   return (
     <>
-      {loading ? (
-        <LoadingOutlined />
-      ) : (
-        <div>
-          <div>
-            <p>hello</p>
-          </div>
-          {/* <h3>{console.log(bookDetails.title)}</h3> */}
+      <div className="container">
+        <div className=" grid-container m-9 gap-3 justify-center items-center text-center">
+          {loading ? (
+            <div className="mr-auto ml-auto col-span-5 h-56 ">
+              <LoadingOutlined />
+            </div>
+          ) : (
+            bookDetails.map((book, index) => (
+              <div key={index}>
+                <div>
+                  <img
+                    src={book.image}
+                    alt={book.title}
+                    className="ml-auto mr-auto "
+                  />
+                  <h2 className="text-black font-semibold">{book.title}</h2>
+                  <button className="bg-green-400 px-5 py-2 rounded-lg text-white">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
