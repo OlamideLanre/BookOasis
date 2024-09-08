e31ebef5e1cc69b50f446a8d285f4e2809262cd3
Akshola00 <akinniyishola07@gmail.com>
2024-09-08 21:50:19 +0100

    refac

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-        <Footer />}
+        <Footer />
       </AuthProvider>
     </>
   );
